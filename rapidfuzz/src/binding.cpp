2a6164013cb007852ba3afaf0397014b0371ca15
Aleksandr Aleksashin <aleksandr.aleksashin@dasha.ai>
2023-01-13 18:40:32 +0700

    add bindings to rapidfuzz

---
+++ rapidfuzz/src/binding.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include "binding.h"
+#include <rapidfuzz/fuzz.hpp>
+
+using namespace std;
+
+extern "C" {
+	ADDExport double _cdecl ratio(char* const s1, char* const s2, double score_cut_off){
+		return rapidfuzz::fuzz::ratio(s1, s2, score_cut_off);
+	}
+	ADDExport double _cdecl token_ratio(char* const s1, char* const s2, double score_cut_off){
+		 return rapidfuzz::fuzz::token_ratio(s1, s2, score_cut_off);
+	}
+	ADDExport double _cdecl partial_ratio(char* const s1, char* const s2, double score_cut_off){
+		return rapidfuzz::fuzz::partial_ratio(s1, s2, score_cut_off);
+	}
+	ADDExport double _cdecl token_set_ratio(char* const s1, char* const s2, double score_cut_off){
+		return rapidfuzz::fuzz::token_set_ratio(s1, s2, score_cut_off);
+	}
+	ADDExport double _cdecl partial_token_ratio(char* const s1, char* const s2, double score_cut_off){
+		return rapidfuzz::fuzz::partial_token_ratio(s1, s2, score_cut_off);
+	}
+	ADDExport double _cdecl partial_token_set_ratio(char* const s1, char* const s2, double score_cut_off){
+		return rapidfuzz::fuzz::partial_token_set_ratio(s1, s2, score_cut_off);
+	}
+	ADDExport double _cdecl partial_token_sort_ratio(char* const s1, char* const s2, double score_cut_off){
+		return rapidfuzz::fuzz::partial_token_sort_ratio(s1, s2, score_cut_off);
+	}
+}
---
+++ rapidfuzz/src/binding.h
@@ -0,0 +1,18 @@
+#pragma once
+#define _cdecl
+#ifdef __declspec(dllexport)
+#define ADDExport __declspec (dllexport)
+#else
+#define ADDExport
+
+#endif
+
+extern "C" {
+	ADDExport double _cdecl ratio(char* const s1, char* const s2, double score_cut_off);
+	ADDExport double _cdecl token_ratio(char* const s1, char* const s2, double score_cut_off);
+	ADDExport double _cdecl partial_ratio(char* const s1, char* const s2, double score_cut_off);
+	ADDExport double _cdecl token_set_ratio(char* const s1, char* const s2, double score_cut_off);
+	ADDExport double _cdecl partial_token_ratio(char* const s1, char* const s2, double score_cut_off);
+	ADDExport double _cdecl partial_token_set_ratio(char* const s1, char* const s2, double score_cut_off);
+	ADDExport double _cdecl partial_token_sort_ratio(char* const s1, char* const s2, double score_cut_off);
+}
